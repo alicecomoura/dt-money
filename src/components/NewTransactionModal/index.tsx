48f6023a07b4274f9b2a49c28919a521a9f102ec
Alice Coelho <alicecomoura@outlook.com>
2022-02-16 21:22:09 -0300

    Creates the entire html structure and its styling

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import { Container, TransactionsTypeContainer, RadioBox } from './styles';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit');
+    
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    placeholder="Título"
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <TransactionsTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType('deposit')}
+                        isActive={type === 'deposit'}
+                        activeColor="green"
+                    >
+                        <img src={income} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType('withdraw')}
+                        isActive={type === 'withdraw'}
+                        activeColor="red"
+                    >
+                        <img src={outcome} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+
+                </TransactionsTypeContainer>
+
+                <input 
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+                
+            </Container>
+        </Modal>
+    )
+}
